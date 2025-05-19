3b37ba45cc8beaf9cf7a124ca2adf508755611e4
testChandu <vlpchandu369@gmail.com>
2025-05-19 11:58:17 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,3 +1,16 @@
 with open("./simplestorage.sol","r")as file;
   simple_storeage_file =file.read()
 print(simple_storeage_file)
+compiled_sol =compled-standard{
+{
+  "language"; "Solidity",
+"sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+"settings": {
+  "outputSelection":{
+    "*":{"*": ["abc","metdata","evm.bytecode","evm.sourceMap"]}
+  }
+  },
+},
+soll_veersion="0.6.0"
+}
+print(compiled_sol)
